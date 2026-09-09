4e5523c9069b044d37ce6d4ab723b63e5426ed18
Naruphu <naruphu@gmail.com>
2026-09-09 17:31:36 +0700

    Fix login redirect after registration

--- src/main/java/controller/LoginServlet.java
+++ src/main/java/controller/LoginServlet.java
@@ -34,7 +34,8 @@ public class LoginServlet extends BaseServlet {
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+		request.getRequestDispatcher("/login.jsp")
+        .forward(request, response);
 	}
 
 	/**
