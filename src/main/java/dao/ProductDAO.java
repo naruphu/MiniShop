b58931ab6248d954ffa6bd15a009f870bfac0622
Naruphu <naruphu@gmail.com>
2026-09-12 15:52:39 +0700

    Fix admin product search HQL

--- src/main/java/dao/ProductDAO.java
+++ src/main/java/dao/ProductDAO.java
@@ -212,7 +212,7 @@ public class ProductDAO implements Dao<Product, Integer> {
 	        .openSession();
 
 
-	        String hql = "FROM Product p";
+	        String hql = "FROM Product p WHERE 1=1";
 	        if(keyword != null && !keyword.trim().isEmpty()) {
 	            hql += " AND p.name LIKE :keyword";
 	        }
